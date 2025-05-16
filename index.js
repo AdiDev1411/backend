1c9a81783801f9736161209204544d2591d51fea
AdiDev1411 <adiforpro@gmail.com>
2025-05-16 18:50:47 +0530

    Port-change

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+
 
 // Middleware
 app.use(cors());
